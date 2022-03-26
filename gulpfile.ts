ce5c67d8f82d5b5c00a333cb179f5250c642a803
Mandar1jn <marijnkneppers@gmail.com>
2022-03-26 17:15:37 +0100

    Migrated gulpfile to typescript and further project setup

---
+++ gulpfile.ts
@@ -0,0 +1,49 @@
+import gulp from "gulp";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
+import tsify from "tsify";
+import connect from 'gulp-connect';
+
+async function buildHTML() {
+	return gulp.src(["index.html"]).pipe(gulp.dest("dest")).pipe(connect.reload());
+}
+
+function buildImage() {
+	return gulp.src('images/*')
+	.pipe(gulp.dest('dest/images'));
+}
+
+async function buildJS() {
+	return browserify({
+		basedir: ".",
+		debug: true,
+		entries: ["scripts/main.ts"],
+		cache: {},
+		packageCache: {},
+	})
+		.plugin(tsify)
+		.bundle()
+		.pipe(source("bundle.js"))
+		.pipe(gulp.dest("dest"))
+		.pipe(connect.reload());
+}
+
+function server() {
+	connect.server({
+		livereload: true,
+		root: "dest/"
+	});
+}
+
+function watch() {
+	gulp.watch(["./index.html"], buildHTML);
+	gulp.watch(["./scripts/*.ts"], buildJS);
+	gulp.watch(["./images/*"], buildImage);
+}
+
+const build = gulp.parallel(buildHTML, buildJS, buildImage);
+
+exports.build = build;
+exports.start = gulp.series(build, gulp.parallel(server, watch));
+
+exports.default = exports.start;
